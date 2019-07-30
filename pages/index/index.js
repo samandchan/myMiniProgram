8a761b09b74ca2e2f69f719ac229a43d9ae0974c
sam <samvip888888@163.com>
2019-07-30 09:52:58 +0800

    分类导航

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,9 @@ import { request } from '../../request/index.js'
 Page({
   data: {
     // 轮播图数据
-    swiperList: []
+    swiperList: [],
+    // 分类导航数据
+    cateList: []
   },
   // // 页面一加载就触发
   // onLoad() {
@@ -16,15 +18,27 @@ Page({
   onLoad() {
     // 获取轮播图数据
     this.getSwiperList()
+    // 分类导航
+    this.getCateList()
   },
   // 定义获取轮播图数据方法
   getSwiperList() {
-    request({url:'/home/swiperdata'})
+    request({url: '/home/swiperdata'})
       .then((result) => {
-        console.log(result);
+        // console.log(result);
         this.setData({
           swiperList: result
         })
       })
+  },
+  // 获取分类导航数据的方法
+  getCateList() {
+    request({url: '/home/catitems'})
+    .then((result) => {
+      console.log(result);
+      this.setData({
+        cateList: result
+      })
+    })
   }
 })
