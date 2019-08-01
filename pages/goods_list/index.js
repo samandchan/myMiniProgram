4a046f5f5d7f4a155425b7cb36abe92524a83810
sam <samvip888888@163.com>
2019-08-01 10:46:44 +0800

    商品列表tab栏

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -5,9 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        title: '综合',
+        isActive: true
+      },
+      {
+        id: 1,
+        title: '销量',
+        isActive: false
+      },
+      {
+        id: 2,
+        title: '价格',
+        isActive: false
+      }
+    ]
   },
-
+  // 子组件触发的事件
+  handleItemChange(e) {
+    // console.log(e);
+    const { index } = e.detail
+    const  tabs  = this.data.tabs
+    tabs.forEach((v, i) => {
+      index === i? v.isActive = true : v.isActive = false
+    })
+    this.setData({
+      tabs
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
