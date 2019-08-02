b1d468c35da2d7182c591a51c605d9b04e403008
sam <samvip888888@163.com>
2019-08-02 09:28:25 +0800

    商品列表-正在等待图标

--- request/index.js
+++ request/index.js
@@ -1,5 +1,12 @@
 // 封装请求接口
+let ajaxTimes = 0;
 export const request = (params) => {
+  ajaxTimes++;
+  // console.log(ajaxTimes);
+  // 显示正在等待图标
+  wx.showLoading({
+    title: '正在加载中'
+  });
   const baseUrl = 'https://api.zbztb.cn/api/public/v1'
   return new Promise((resolve, reject) => {
     var reqTask = wx.request({
@@ -10,6 +17,12 @@ export const request = (params) => {
       },
       fail: (error)=>{
         reject(error)
+      },
+      complete: ()=> {
+        ajaxTimes--
+        if(ajaxTimes === 0) {
+          wx.hideLoading();
+        }
       }
     });
   })
