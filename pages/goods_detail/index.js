88734df468cab4b3ef8cd9585c2c80b152177c13
sam <samvip888888@163.com>
2019-08-02 11:41:38 +0800

    商品列表-轮播图

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,40 @@
-// pages/goods_detail/index.js
+import {request} from '../../request/index.js'
+import regeneratorRuntime from '../../lib/runtime/runtime'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goodsInfo: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(options) {
+    // console.log(options);
+    this.getGoodsInfo(options.goods_id)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取商品详情
+  async getGoodsInfo(goods_id) {
+    const result = await request({
+      url: '/goods/detail',
+      data: {goods_id}
+    })
+    // console.log(result);
+    this.setData({
+      goodsInfo: {
+        goods_name: result.goods_name,
+        goods_price: result.goods_price,
+        goods_introduce: result.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: result.pics
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 预览图片
+  handlePreviewImage(e) {
+    console.log(e);
+    const { index } = e.currentTarget.dataset
+    const urls = this.data.goodsInfo.pics.map(v=>v.pics_big)
+    wx.previewImage({
+      current: urls[index],
+      urls
+    });
+    
   }
+
+
 })
