17681110904bfe4ab6eb4ec8b64644ba58ab29f9
sam <samvip888888@163.com>
2019-08-05 21:03:11 +0800

    个人中心-静态页面

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,7 +1,11 @@
 // pages/login/index.js
 Page({
   handleGetUserInfo(e) {
-    console.log(e);
-    
+    // console.log(e);
+    const { userInfo } = e.detail
+    wx.setStorageSync('userInfo', userInfo);
+    wx.navigateBack({
+      delta: 1
+    })
   }
 })
